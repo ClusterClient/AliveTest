9f2bc93bdc4c3aedade90fa130fdce7b4e58b556
node1 <askjdflkja>
2015-02-19 16:15:24 -0500

    random nuber added

--- alive.sh
+++ alive.sh
@@ -1,7 +1,7 @@
 git config --global user.email "askjdflkja"
 git config --global user.name "node1"
 git remote set-url origin https://ClusterClient:Cluster123@github.com/ClusterClient/AliveTest.git
-while [true]
+while true;
 do
 git add *
 git commit -m"random nuber added"
