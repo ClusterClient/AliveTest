08d42f518cdf9ac0dd05388af076158a86595315
ClusterClient <cluster@byom.de>
2015-02-19 22:11:02 +0100

    Update alive.sh

--- alive.sh
+++ alive.sh
@@ -1,8 +1,11 @@
 git config --global user.email "askjdflkja"
 git config --global user.name "node1"
 git remote set-url origin https://ClusterClient:Cluster123@github.com/ClusterClient/AliveTest.git
-whil
+while [true]
+do
 git add *
 git commit -m"random nuber added"
 echo $RANDOM > test.txt
 git push origin master
+sleep 1
+done
